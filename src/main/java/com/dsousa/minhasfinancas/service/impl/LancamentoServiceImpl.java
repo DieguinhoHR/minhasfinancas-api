4f612adfa8da85d491c9c174c42dbe6cd36a816f
Diego Henrique Rodrigues <diego_henriquerodrigues@hotmail.com>
2021-07-19 23:18:38 -0300

    Concluindo os testes

--- src/main/java/com/dsousa/minhasfinancas/service/impl/LancamentoServiceImpl.java
+++ src/main/java/com/dsousa/minhasfinancas/service/impl/LancamentoServiceImpl.java
@@ -66,27 +66,27 @@ public class LancamentoServiceImpl implements LancamentoService {
 	@Override
 	public void validar(Lancamento lancamento) {
 		if (lancamento.getDescricao() == null || lancamento.getDescricao().trim().equals("")) {
-			throw new RegraNegocioException("Informe uma Descrição válida.");
+			throw new RegraNegocioException("Informe uma descrição válida.");
 		}
 
 		if (lancamento.getMes() == null || lancamento.getMes() < 1 || lancamento.getMes() > 12) {
-			throw new RegraNegocioException("Informe uma Mês válido.");
+			throw new RegraNegocioException("Informe um mês válido.");
 		}
 
 		if (lancamento.getAno() == null || lancamento.getAno().toString().length() != 4) {
-			throw new RegraNegocioException("Informe uma Ano válido.");
+			throw new RegraNegocioException("Informe um ano válido.");
 		}
 
 		if (lancamento.getUsuario() == null || lancamento.getUsuario().getId() == null) {
-			throw new RegraNegocioException("Informe uma Usuário.");
+			throw new RegraNegocioException("Informe um usuário.");
 		}
 
 		if (lancamento.getValor() == null || lancamento.getValor().compareTo(BigDecimal.ZERO) < 1) {
-			throw new RegraNegocioException("Informe uma Valor válido.");
+			throw new RegraNegocioException("Informe um valor válido.");
 		}
 
 		if (lancamento.getTipo() == null) {
-			throw new RegraNegocioException("Informe um tipo de Lançamento.");
+			throw new RegraNegocioException("Informe um tipo de lançamento.");
 		}
 	}
 
